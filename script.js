e76558c0b3b1ada12f4e4193ac40790f111925c3
MC <145706726+sirtostito@users.noreply.github.com>
2024-10-17 15:35:00 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // Counter App
 let count = document.getElementById("count-el")
-let plusButton = document.getElementById("enterButton")
-let saveButton = document.getElementById("saveButton")
-let saveScript = document.createElement("h2")
-saveScript.innerHTML = "Last saved value(s): "
-document.body.appendChild(saveScript)
+let plusButton = document.getElementById("increment-btn")
+let saveButton = document.getElementById("save-btn")
+let saveScript = document.createElement("p")
+saveScript.innerHTML = "Saved value(s): "
 function increment() {
     count++
     document.getElementById("count-el").innerHTML = count
 }
 function save() {
-    saveScript.innerHTML += count + " "
+    saveScript.innerHTML += count + " - "
+    document.getElementById("save-el").innerHTML = saveScript.innerHTML
 }
